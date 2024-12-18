9f0b173d746720b1e9e8435600ed823dee471b3e
snimmagadda1 <saiguy@me.com>
2024-12-18 15:50:49 -0500

    Add dev mode check

--- rxdb-server/database.ts
+++ rxdb-server/database.ts
@@ -5,11 +5,9 @@ import {
   type RxCollectionCreator,
 } from "rxdb";
 import { getRxStorageMemory } from "rxdb/plugins/storage-memory";
-import { formatISO, isToday, startOfDay } from "date-fns";
 import {
   EVENTS_SCHEMA,
   type RxEventsDatabase,
-  type RxEventDocumentType,
   USER_SCHEMA_LITERAL,
   type RxUserDocumentType,
 } from "./schema";
@@ -23,33 +21,13 @@ const collectionSettings = {
   } as RxCollectionCreator<any>,
 };
 
-const initWeek = () => {
-  const currentDate = new Date();
-  const currentDayOfWeek = currentDate.getDay();
-  const startOfWeek = new Date(currentDate);
-  startOfWeek.setDate(currentDate.getDate() - currentDayOfWeek);
-  const endOfWeek = new Date(startOfWeek);
-  endOfWeek.setDate(startOfWeek.getDate() + 6);
-
-  const week = [];
-
-  for (let i = 0; i < 7; i++) {
-    const dayDate = new Date(startOfWeek);
-    dayDate.setDate(startOfWeek.getDate() + i);
-    week.push({
-      date: startOfDay(dayDate),
-      isCurrent: isToday(dayDate),
-    });
-  }
-
-  return week;
-};
-
 export async function createDb(): Promise<RxEventsDatabase> {
   // TODO: check dev mode
-  await import("rxdb/plugins/dev-mode").then((module) =>
-    addRxPlugin(module.RxDBDevModePlugin)
-  );
+  if (process.env.NODE_ENV !== "production") {
+    await import("rxdb/plugins/dev-mode").then((module) =>
+      addRxPlugin(module.RxDBDevModePlugin)
+    );
+  }
 
   const storage = getRxStorageMemory();
 
@@ -65,29 +43,6 @@ export async function createDb(): Promise<RxEventsDatabase> {
   await db.addCollections(collectionSettings);
 
   console.log("DatabaseService: create collections");
-
-  const week = initWeek();
-  await db.events.bulkInsert(
-    [
-      "A demo event",
-      "Hover me to mark as complete",
-      "This one has a color",
-    ].map(
-      (title, idx) =>
-        ({
-          id: "event-" + idx,
-          title,
-          date: formatISO(startOfDay(week[idx].date), {
-            representation: "complete",
-          }),
-          index: 0,
-          completed: false,
-          notes: "",
-          timestamp: new Date().getTime(),
-        } as RxEventDocumentType)
-    )
-  );
-  console.log("DatabaseService: bulk insert events");
 
   const testUser = {
     id: "test-user",
