1330ea35079071205084c5492745b30ab1c513f9
sai <sai.nimmagadda@optum.com>
2024-12-26 13:31:30 -0600

    base refactor to simple folder structure

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { createDb, setupServer } from "./rxdb-server"
+import type { Express } from "express";
+import { setupAuth, MEMORY_STORE } from "./middleware";
+import authRoutes from "./routes/auth";
+import { setupMiddleware } from "./middleware";
+
+// Create RxDB instance
+const db = await createDb();
+
+// Setup rx-server instance, w/ session middleware config
+const rxServer = await setupServer(db, MEMORY_STORE);
+
+// Access the underlying Express app
+const app = rxServer.serverApp as Express;
+
+// Init passport with strategy
+setupAuth(db);
+
+// Init middleware (session, CORS, passport, logging)
+setupMiddleware(app, db);
+
+// Use configured auth handler routes
+app.use("/auth", authRoutes);
+
+await rxServer.start();
---
+++ src/middleware/index.ts
@@ -0,0 +1,6 @@
+
+export * from "./cors";
+export * from "./logger";
+export * from "./passport";
+export * from "./session";
+export * from "./setup";
